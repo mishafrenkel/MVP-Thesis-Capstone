d008c2a5a8bc6ecf43989b76af22b4eda66493cb
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-04 18:22:42 -0700

    merge with master branch and add styles-components to  ListItem

--- hack-react-news/src/components/List/styles.js
+++ hack-react-news/src/components/List/styles.js
@@ -2,4 +2,10 @@ import styled from 'styled-components';
 
 export const ListWrapper = styled.ul`
   background-color: ${({ theme }) => theme.backgroundSecondary};
+  border-radius: 4px;
+  margin-left: auto;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
 `;
+
---
+++ hack-react-news/src/components/ListItem/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Item, Title, Host, ExternalLink, Description, CommentLink } from './styles';
+
+const LINK_REL = 'nofollow noreferrer noopener';
+
+const ListItem = () => {
+    return (
+        <Item>
+            <ExternalLink href='https://github.com' rel={LINK_REL} target="_blank">
+                <Title>
+                    The Developer Community <Host>{github.com}</Host>
+                </Title>
+            </ExternalLink>
+            <Description>
+                5000 points by{' '}
+                <CommentLink href="#" rel={LINK_REL} target="_blank">
+                    Test User
+                </CommentLink>{' '}
+                1 Hour Ago {' | '}
+                <CommentLink href="#" rel={LINK_REL} target="_blank">
+                    40 Comments
+                </CommentLink>
+            </Description>
+        </Item>
+    );
+};
+
+export default ListItem;
