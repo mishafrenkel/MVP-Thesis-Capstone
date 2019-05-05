d3a186013ef572bb5388135368bed2b8757bc7aa
Michael Frenkel <michael.frenkel@outlook.com>
2019-05-04 17:08:46 -0700

    registerServiceWorker forserving assets from local cache

--- hack-react-news/src/index.js
+++ hack-react-news/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'store';
 import App from 'components/App';
 import GlobalStyles from 'styles/globals';
+import registerServiceWorker from './registerServiceWorker';
+
+
+
 const renderApp = () => {
+  // No intial state for now, pass in empty object for state
+  const store = configureStore({});
 
   if (process.env.NODE_ENV !== 'production') {
-    console.log('Initial state ->');
+    console.log('Initial state printing ->');
+    console.log(store.getState());
   }
 
   ReactDOM.render(
--- hack-react-news/src/store/app/reducer.js
+++ hack-react-news/src/store/app/reducer.js
@@ -8,6 +8,7 @@ const app = (state = getInitialState(), { type, payload }) => {
     switch (type) {
         case actionTypes.SET_THEME: 
          return {
+         // the keys of the state will be replaced by the keys in ...state that match — in this case theme.
              ...state, 
              ...payload,
          } ;
