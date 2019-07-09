81e56b3ceab3615d18552a2a3e64e307355beac2
Michael Frenkel <michael.frenkel@outlook.com>
2019-07-09 15:37:59 -0700

    Updates

---
+++ precache-manifest.a2d39429b833c4d92f1a415e13e08978.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "28c7ab503373c81e0da0",
+    "url": "/MVP-Thesis-Capstone/static/js/runtime~main.28c7ab50.js"
+  },
+  {
+    "revision": "673ef41afdae6284f1da",
+    "url": "/MVP-Thesis-Capstone/static/js/main.673ef41a.chunk.js"
+  },
+  {
+    "revision": "85f9da0e212b7dbe02cb",
+    "url": "/MVP-Thesis-Capstone/static/js/1.85f9da0e.chunk.js"
+  },
+  {
+    "revision": "c9c791d70e47a5c5585bd7320b217204",
+    "url": "/MVP-Thesis-Capstone/index.html"
+  }
+];
